3a63a04959c08aa89bdbc61c947b667ca350d1ef
kristian <krispetter@gmail.com>
2020-07-25 20:39:00 +0200

    c

--- database/Databaseconnector.py
+++ database/Databaseconnector.py
@@ -4,20 +4,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db_username = os.environ["DB_USERNAME"]
-db_password = os.environ["DB_PASSWORD"]
-db_host     = os.environ["DB_HOST"]
-db_database = os.environ["DB_DATABASE"]
-
-url = f"mysql+pymysql://{db_username}:{db_password}@{db_host}:3306/{db_database}"
-
 Base = declarative_base()
 
+def default_uri():
+
+	db_username = os.environ.get("DB_USERNAME")
+	db_password = os.environ.get("DB_PASSWORD")
+	db_host     = os.environ.get("DB_HOST")
+	db_database = os.environ.get("DB_DATABASE")
+
+	url = f"mysql+pymysql://{db_username}:{db_password}@{db_host}:3306/{db_database}"
+
+
 class DiscountKoalaDatabase:
-	def __init__(self, url=url):
-		engine = create_engine(url)
-		self.Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+	def __init__(self, uri=default_uri()):
+		engine = create_engine(uri)
 
+		Base.metadata.create_all(bind=engine)
 
-	def create_session(self):
-		return self.Session()
+		Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+		self.session = self.Session()
