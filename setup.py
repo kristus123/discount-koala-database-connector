e18f28bdd8054f9e9e1667053d16fd27723eefd2
kristian <krispetter@gmail.com>
2020-07-25 19:20:04 +0200

    c2

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="discountKoala",
+    version="0.0.1",
+    author="todo",
+    author_email="todo",
+    description="todo",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
