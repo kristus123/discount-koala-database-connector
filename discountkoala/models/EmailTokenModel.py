fbda06ec49eeff29fe055d5cd83148e121589630
kristus123 <krispetter@gmail.com>
2020-08-03 20:22:12 +0200

    Update EmailTokenModel.py

--- discountkoala/models/EmailTokenModel.py
+++ discountkoala/models/EmailTokenModel.py
@@ -9,6 +9,8 @@ class EmailTokenModel(Base):
 	email_token_id = Column("email_token_id", Integer, primary_key=True)
 	user_id = Column("user_id", Integer)
 	
+	full_email_name = Column(String(100), nullable=False)
+	
 	email_provider = Column(String(40), nullable=False)
 
 	access_token = Column(String(500), nullable=False)
